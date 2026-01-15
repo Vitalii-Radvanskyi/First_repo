203d11dce21411eba34ef135e2c9f4c98f734171
Vitalii Radvanskyi <radvanskijvitalij@gmail.com>
2026-01-14 18:39:42 -0700

    main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 num = int(input("Enter the number"))
-if num >=  25:
+isHesCar = True
+if num >=  25 or not isHesCar :
     print("Number is bigger then 25")
     if num == 27:
         print("Lucky number")
 elif num == 1: 
-    print("Oleg\'s lucky number")        
+    print("Oleg\'s lucky number")
+else:
+    print("noooooo")        
