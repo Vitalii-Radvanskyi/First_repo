7591711aa3ba93378eed069e1311c2c2de6e0c85
Vitalii Radvanskyi <radvanskijvitalij@gmail.com>
2026-01-15 13:40:04 -0700

    main.py

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 num = int(input("Enter the number"))
 isHesCar = True
 if num >=  25 or not isHesCar :
-    print("Number is bigger then 25")
+   # print("Number is bigger then 25")
     if num == 27:
-        print("Lucky number")
+       # print("Lucky number")
 elif num == 1: 
-    print("Oleg\'s lucky number")
+   # print("Oleg\'s lucky number")
 else:
-    print("noooooo")      
-      
+   # print("noooooo")      
+    
+text = input('Напиши щось')
+text = text.capitalize()
+#print(text)
+text = input("Напиши щось")
+#print(text.lstrip("-/*+"))
+ 
