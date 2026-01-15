4d2a03f6e3a25f164276bb47f1ea4c29cb008041
Vitalii Radvanskyi <radvanskijvitalij@gmail.com>
2026-01-14 17:41:09 -0700

    main.py

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 num = int(input("Enter the number"))
-if num >=  27:
-    print("Number is bigger then 27")
-    
+if num >=  25:
+    print("Number is bigger then 25")
+    if num == 27:
+        print("Lucky number")
+elif num == 1: 
+    print("Oleg\'s lucky number")        
