59d34d5237de94eb57638cb26502d3c294f3412d
Vitalii Radvanskyi <radvanskijvitalij@gmail.com>
2026-01-18 14:01:30 -0700

    main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-price_sandwich = 3.27 
-price_water = 1.18
-price_apple = 0.86
-num_sandwich = price_sandwich * 4
-num_water = price_water * 3
-num_apple = price_apple * 5
-sum = float(num_sandwich + num_water + num_apple)
-print("Повних доларів:", sum )
-print("Загальна вартість у центах:", sum * 100)
+price_burger = 5.49
+price_cola = 1.99
+num_burger = int(input("Кількість Бургерів:"))
+num_cola = int(input("Кількість Коли:"))
+cost_no_GST = num_burger * price_burger + num_cola * price_cola
+GST = float((cost_no_GST) * 0.05)
+total_cost = cost_no_GST + GST
+full_dolars = int(total_cost)
+total_cents = int(total_cost * 100)
+print("Повних доларів:", full_dolars)
+print("Загальна вартість у центах:", total_cents)
