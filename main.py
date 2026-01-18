a51b64f836dcfea78a38bf73a334c8d1b70f338b
Vitalii Radvanskyi <radvanskijvitalij@gmail.com>
2026-01-18 13:18:20 -0700

    main.py

--- main.py
+++ main.py
@@ -1,17 +1,9 @@
-num = int(input("Enter the number"))
-isHesCar = True
-if num >=  25 or not isHesCar :
-   # print("Number is bigger then 25")
-    if num == 27:
-       # print("Lucky number")
-elif num == 1: 
-   # print("Oleg\'s lucky number")
-else:
-   # print("noooooo")      
-    
-text = input('Напиши щось')
-text = text.capitalize()
-#print(text)
-text = input("Напиши щось")
-#print(text.lstrip("-/*+"))
- 
+price_sandwich = 3.27 
+price_water = 1.18
+price_apple = 0.86
+num_sandwich = price_sandwich * 4
+num_water = price_water * 3
+num_apple = price_apple * 5
+sum = float(num_sandwich + num_water + num_apple)
+print("Повних доларів:", sum )
+print("Загальна вартість у центах:", sum * 100)
