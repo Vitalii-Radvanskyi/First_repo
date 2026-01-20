31a5e16b178f4cea44fe67f416b039cf4fc54095
Vitalii Radvanskyi <radvanskijvitalij@gmail.com>
2026-01-19 17:00:46 -0700

    myPython.py

--- myPython.py
+++ myPython.py
@@ -1,29 +1,68 @@
-data = 'info'
-if data == 'info':
-    correct = True
-else:
-    correct = False
-print(correct)   
-data = 'info'
-correct = True if data == 'info' else False
-print(correct)
-print("\n\n\n")
+nums = [5, 10, 15, 20, 25, 30]
+for i in range(len(nums)-1, -1, -1):
+    if nums[i] % 5 != 0 or nums[i] % 10 != 0:
+        nums.remove(nums[i])
+print(nums)    
 
-for i in range(0, 100, 10):
-         print(i)
-print("\n\n\n")
-word = 'Наташа'   
-for i in word:
-    if i == 'т':
-        print('Літера Т є в цьому слові')
-        break
-    else:
-        print("в цьому слові немає літери Т")
-              
-for i in range(1, 11):
-    if i % 2 == 0:
-        continue
-    if i == 7:
-        break
-    print("el:", i )
-        
+letters = ['a', 'b', 'c', 'd', 'e']
+for i in range(len(letters)):
+    if i % 2 != 0:
+        letters[i] = 'X'
+print(letters) # Невдалось розвязати задачу ще повернусь 
+  
+nums = [1, 2, 3, 2, 4, 2, 5]
+for i in range(len(nums)-1, -1, -1):
+    if nums [i] != 2:
+        nums.remove(nums[i])
+print(len(nums))
+
+nums = [4, 7, 10, 13, 16, 19, 22]
+for i in range(len(nums)-1, -1, -1):
+    if i % 2 != 0:
+        nums.remove(nums[i])
+print(nums)       
+
+letters = ['p', 'y', 't', 'h', 'o', 'n']
+for i in range(len(letters)):
+    if i % 2 != 0:
+        letters[i] = '*'
+print(letters)
+
+my_dict = {"name": "Аліна", "age": 24, "city": "Калгарі"}
+my_dict["age"] = 26  # Змінює вік на 26
+my_dict["email"] = "alice@example.com"  # Додає нову пару ключ-значення
+del my_dict["city"]
+print("name" in my_dict)
+
+car = {
+    "brand": "Toyota",
+    "year": 2020
+}
+color = car.get("color", "Unknown") 
+print(color)
+
+person = {
+    "name": "John",
+    "age": 40,
+    "job": "Engineer"
+}
+for key, value in person.items():
+    print(key, "-", value)
+    
+scores = {
+    "math": 80,
+    "english": 65,
+    "physics": 90
+}
+for key, value in scores.items():
+    if value >=70:
+        print(key, value)    # результвт є, але невпевнений чи код виглядає коректно
+
+orders = [
+    {"id": 1, "paid": True},
+    {"id": 2, "paid": False},
+    {"id": 3, "paid": True}
+]
+for order in orders:
+    if order["paid"]:
+        print(order["id"])
