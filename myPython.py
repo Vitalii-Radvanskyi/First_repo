33483310e3ca4ec4a3da7b1174e930aa6fbd439c
Vitalii Radvanskyi <radvanskijvitalij@gmail.com>
2026-01-30 14:43:59 -0700

    myPython.py

---
+++ myPython.py
@@ -0,0 +1,26 @@
+import math
+print(math.sqrt(81))
+
+import math
+print(math.pow(2, 5))
+
+import math 
+print(math.ceil(7.8))
+print(math.floor(7.8))
+
+
+import math 
+print(math.log(64, 2))
+
+import math
+print(math.radians(180))
+
+import math
+angle = math.radians(30)
+print(math.sin(angle))
+
+import math
+old_price = 200
+new_price = 250
+log_return = math.log(new_price / old_price) 
+print(log_return)
