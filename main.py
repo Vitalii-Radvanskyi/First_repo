fd581b116b4ecb93b5d29006cc0956ab2574e92f
Vitalii Radvanskyi <radvanskijvitalij@gmail.com>
2026-01-31 20:21:31 -0700

    main.py

--- main.py
+++ main.py
@@ -1,10 +1,56 @@
-from datetime import datetime
+from datetime import datetime, date, timedelta
 
-# Створення об'єкта datetime
-now = datetime.now()
 
-# Отримання номера дня тижня
-day_of_week = now.weekday()
+def string_to_date(date_string):
+    return datetime.strptime(date_string, "%Y.%m.%d").date()
 
-# Поверне число від 0 (понеділок) до 6 (неділя)
-print(f"Сьогодні: {day_of_week}")  
+
+def date_to_string(date):
+    return date.strftime("%Y.%m.%d")
+
+
+def prepare_user_list(user_data):
+    prepared_list = []
+    for user in user_data:
+        prepared_list.append({"name": user["name"], "birthday": string_to_date(user["birthday"])})
+    return prepared_list
+
+
+def find_next_weekday(start_date, weekday):
+    days_ahead = weekday - start_date.weekday()
+    if days_ahead <= 0:
+        days_ahead += 7
+    return start_date + timedelta(days=days_ahead)
+
+
+def adjust_for_weekend(birthday):
+    if birthday.weekday() >= 5:
+        return find_next_weekday(birthday, 0)
+    return birthday
+
+
+def get_upcoming_birthdays(users, days=7):
+    upcoming_birthdays = []
+    today = date.today()
+
+    for user in users:
+        birthday_this_year = user["birthday"].replace(year=today.year)
+        if birthday_this_year < today:
+            birthday_this_year = birthday_this_year.replace(year=today.year+1)
+            
+       
+        
+            
+
+        if 0 <= (birthday_this_year - today).days <= days:
+            congratulation_date = birthday_this_year
+            if congratulation_date.weekday()== 5:
+                congratulation_date+=timedelta(days=2)
+            elif congratulation_date.weekday() == 6:
+                    congratulation_date+=timedelta(days =1)
+          
+            
+
+            congratulation_date_str = date_to_string(congratulation_date)
+            upcoming_birthdays.append({"name": user["name"], "congratulation_date": congratulation_date_str})
+    return upcoming_birthdays
