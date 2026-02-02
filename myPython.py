6ace7fb7e950332b6abfbaa7ceab6534cc51eeea
Vitalii Radvanskyi <radvanskijvitalij@gmail.com>
2026-02-02 09:07:54 -0700

    myPython.py

--- myPython.py
+++ myPython.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 def get_days_from_today(date_str):
     try:
-        date_objc = datetime.strptime(date_str, "%Y.%m.%d")
+        date_objc = datetime.strptime(date_str, "%Y-%m-%d")
         time_today = datetime.today()
         deff_time = time_today - date_objc
         return deff_time.days
@@ -11,8 +11,29 @@ def get_days_from_today(date_str):
 
 import random
 def get_number_ticket(min, max, quantity):
+    if min < 0:
+        return []
+    if min >= max:
+        return[]
+    if quantity > (max - min):
+        return[]
     try:
         nums = range(min, max) 
         return random.sample(nums, quantity)
     except (TypeError, ValueError):
-        return None
+        return []
+    
+    
+import re
+def phone_number(numbers):
+    result =[]
+    for digits in numbers:
+        digits = re.sub(r"\D", "", digits)
+        if digits.startswith("380"):
+            result.append("+" + digits)
+        elif digits.startswith("0"):
+            result.append("+38" + digits)
+        else:
+            result.append("+" + digits)
+    return result        
+        
